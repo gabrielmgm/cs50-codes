615ac1d052aac9b72a70a61bbdf70d936738190e
Gabriel Monteiro <monteirogomesgabriel@gmail.com>
2024-11-04 19:55:59 -0300

    Update dictionary.c

--- week 5/dictionary.c
+++ week 5/dictionary.c
@@ -5,57 +5,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
-// Represents a node in a hash table
+// Defines a node in the hash table
 typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
 } node;
 
-// Number of buckets in hash table
-const unsigned int N = 26;
+// Prototypes
+bool find_word(node *head, const char *word);
+void free_list(node *head);
 
-// Hash table
-node *table[N];
+// Keeps track of the number of words in the dictionary
+int total_words = 0;
 
-// Keep track of number of words in dictionary
-unsigned int word_count = 0;
+// Number of buckets in the hash table
+const unsigned int NUM_BUCKETS = 26;
 
-// Returns true if word is in dictionary, else false
+// Hash table, organized by first letter and word length
+node *hash_table[NUM_BUCKETS][LENGTH];
+
+// Checks if a word is in the dictionary, returns true if found, else false
 bool check(const char *word)
 {
-    // Get hash value for word
-    unsigned int index = hash(word);
-
-    // Access linked list at the index
-    node *cursor = table[index];
-
-    // Traverse linked list
-    while (cursor != NULL)
-    {
-        // Compare words case insensitively
-        if (strcasecmp(cursor->word, word) == 0)
-        {
-            return true;
-        }
-        cursor = cursor->next;
-    }
-    return false;
+    int index = hash(word);
+    int length = strlen(word) - 1;
+    node *current = hash_table[index][length];
+    return find_word(current, word);
 }
 
-// Hashes word to a number
+// Searches for a word in a linked list, returns true if found, else false
+bool find_word(node *head, const char *word)
+{
+    if (head == NULL)
+    {
+        return false;
+    }
+    if (strcasecmp(head->word, word) == 0)
+    {
+        return true;
+    }
+    return find_word(head->next, word);
+}
+
+// Hashes a word to an index based on its first letter
 unsigned int hash(const char *word)
 {
-    // Simple hash based on the first letter (case-insensitive)
     return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
+    // Initialize the hash table with NULL values
+    for (int i = 0; i < NUM_BUCKETS; i++)
+    {
+        for (int j = 0; j < LENGTH; j++)
+        {
+            hash_table[i][j] = NULL;
+        }
+    }
+
     // Open the dictionary file
     FILE *file = fopen(dictionary, "r");
     if (file == NULL)
@@ -63,31 +77,40 @@ bool load(const char *dictionary)
         return false;
     }
 
-    // Buffer to hold a word
-    char word[LENGTH + 1];
+    // Buffer to hold each word
+    char word_buffer[LENGTH + 1];
 
-    // Read words from the file one at a time
-    while (fscanf(file, "%45s", word) != EOF)
+    // Read each word from the file
+    while (fscanf(file, "%s", word_buffer) != EOF)
     {
-        // Create a new node
+        // Hash the word to get the index
+        int index = hash(word_buffer);
+        int length = strlen(word_buffer) - 1;
+
+        // Create a new node for the word
         node *new_node = malloc(sizeof(node));
         if (new_node == NULL)
         {
             return false;
         }
 
-        // Copy word into node
-        strcpy(new_node->word, word);
+        // Copy word to the new node and set its next pointer to NULL
+        strcpy(new_node->word, word_buffer);
+        new_node->next = NULL;
 
-        // Hash the word to get index
-        unsigned int index = hash(word);
-
-        // Insert node into hash table (at the beginning of the linked list)
-        new_node->next = table[index];
-        table[index] = new_node;
+        // Insert the node at the beginning of the list in the hash table
+        if (hash_table[index][length] == NULL)
+        {
+            hash_table[index][length] = new_node;
+        }
+        else
+        {
+            new_node->next = hash_table[index][length];
+            hash_table[index][length] = new_node;
+        }
 
         // Increment word count
-        word_count++;
+        total_words++;
     }
 
     // Close the dictionary file
@@ -95,26 +118,36 @@ bool load(const char *dictionary)
     return true;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+// Returns the number of words in the dictionary if loaded, else 0
 unsigned int size(void)
 {
-    return word_count;
+    return total_words;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
+// Unloads the dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // Iterate over each bucket
-    for (int i = 0; i < N; i++)
+    // Free all linked lists in each bucket of the hash table
+    for (int i = 0; i < NUM_BUCKETS; i++)
     {
-        // Free linked list in each bucket
-        node *cursor = table[i];
-        while (cursor != NULL)
+        for (int j = 0; j < LENGTH; j++)
         {
-            node *tmp = cursor;
-            cursor = cursor->next;
-            free(tmp);
+            if (hash_table[i][j] != NULL)
+            {
+                free_list(hash_table[i][j]);
+            }
         }
     }
     return true;
+}
+
+// Recursively frees a linked list
+void free_list(node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    free_list(head->next);
+    free(head);
 }
