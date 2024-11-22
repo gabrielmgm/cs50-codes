33bb6c7ebb5fb54b8a8cb8c382d5231166639845
Gabriel Monteiro <monteirogomesgabriel@gmail.com>
2024-11-21 22:00:51 -0300

    Update mario.py

--- week 6/mario.py
+++ week 6/mario.py
@@ -1,16 +1,20 @@
 def main():
     # Prompt the user for a valid height
     while True:
-        height = int(input("Height: "))
-        if height > 0:
-            break
-        
+        try:
+            height = int(input("Height: "))
+            if 1 <= height <= 8:
+                break
+        except ValueError:  # Handle non-integer input gracefully
+            pass
+
     # Print bricks with the specified height
     print_blocks(height)
 
 # Function to print bricks in a pyramid shape
 def print_blocks(height):
-    for i in range(1, height+1):
+    for i in range(1, height + 1):
+        # Print spaces and bricks for each row
         print(" " * (height - i) + "#" * i)
-        
+
 main()
